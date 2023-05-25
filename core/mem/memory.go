6ce32554539d31e06b3e3ed2b182ebaaec120d0d
javier <quan.nguyen20@onemount.com>
2023-05-25 19:01:18 +0700

    fix: store in mem by store by parent before

--- core/mem/memory.go
+++ core/mem/memory.go
@@ -9,21 +9,45 @@ type Mem interface {
 	Set(key string, value val.EqlValue)
 	Get(key string) val.EqlValue
 	GetParent() Mem
+	Level() uint
 }
 
 type _mem struct {
 	m      sync.Map
 	parent Mem
+	level  uint
 }
 
 func NewMemory(parent Mem) Mem {
+	var level uint
+	if parent == nil {
+		level = 0
+	} else {
+		level = parent.Level() + 1
+	}
 	return &_mem{
 		parent: parent,
+		level:  level,
 	}
 }
 
 func (m *_mem) Set(key string, value val.EqlValue) {
-	m.m.Store(key, value)
+	level := m.Level()
+	m.set(key, value, level)
+}
+
+func (m *_mem) set(key string, value val.EqlValue, level uint) {
+	p := m.parent
+	if p != nil {
+		if p.Get(key) != nil {
+			p.Set(key, value)
+			return
+		}
+		p.(*_mem).set(key, value, level)
+	}
+	if m.Level() == level {
+		m.m.Store(key, value)
+	}
 }
 
 func (m *_mem) Get(key string) val.EqlValue {
@@ -40,4 +64,8 @@ func (m *_mem) Get(key string) val.EqlValue {
 
 func (m *_mem) GetParent() Mem {
 	return m.parent
+}
+
+func (m *_mem) Level() uint {
+	return m.level
 }
