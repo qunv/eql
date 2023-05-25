c1bf6478d29743596b4c3f22835beced188a0972
javier <quan.nguyen20@onemount.com>
2023-05-25 09:00:44 +0700

    fix:main

--- cmd/eql.go
+++ cmd/eql.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/csv"
+	"flag"
 	"github.com/qunv/eql"
 	"log"
 	"os"
@@ -12,37 +13,37 @@ var eqlRegex = regexp.MustCompile("^[a-zA-z0-9].+\\.eql$")
 
 func main() {
 
-	//subRun := flag.NewFlagSet("run", flag.ExitOnError)
-	//csvPath := subRun.String("csv", "", "data for our program")
-	//
-	//if len(os.Args) < 2 {
-	//	log.Fatal("expected 'run' subcommands")
-	//}
+	subRun := flag.NewFlagSet("run", flag.ExitOnError)
+	csvPath := subRun.String("csv", "", "data for our program")
 
-	//switch os.Args[1] {
-	//case "run":
-	//	err := subRun.Parse(os.Args[3:])
-	//	if err != nil {
-	//		log.Fatal(err.Error())
-	//	}
-	//	executedFile := os.Args[2]
-	//	if !eqlRegex.MatchString(executedFile) {
-	//		log.Fatal("no eql file found")
-	//	}
-	//	if executedFile == "" {
-	//		log.Fatal("no executed file")
-	//	}
-	//	if csvPath == nil || *csvPath == "" {
-	//		log.Fatal("no csv file")
-	//	}
-		data := initData("/home/joseph/Documents/self/eql/cmd/test.csv")
-		parser := eql.NewEqlParser(data)
-		if err := parser.Execf("/home/joseph/Documents/self/eql/cmd/main.eql"); err != nil {
+	if len(os.Args) < 2 {
+		log.Fatal("expected 'run' subcommands")
+	}
+
+	switch os.Args[1] {
+	case "run":
+		err := subRun.Parse(os.Args[3:])
+		if err != nil {
 			log.Fatal(err.Error())
 		}
-	//default:
-	//	log.Fatal()
-	//}
+		executedFile := os.Args[2]
+		if !eqlRegex.MatchString(executedFile) {
+			log.Fatal("no eql file found")
+		}
+		if executedFile == "" {
+			log.Fatal("no executed file")
+		}
+		if csvPath == nil || *csvPath == "" {
+			log.Fatal("no csv file")
+		}
+		data := initData(*csvPath)
+		parser := eql.NewEqlParser(data)
+		if err := parser.Execf(executedFile); err != nil {
+			log.Fatal(err.Error())
+		}
+	default:
+		log.Fatal()
+	}
 
 }
 
